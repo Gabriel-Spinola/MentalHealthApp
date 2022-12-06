6188017e681c2b2d497f3c91b28e2896ea1cc241
gseven-gdv <zerogravitystd@gmail.com>
2022-12-06 14:57:27 -0300

    card blue part

--- lib/widgets/ticket_view.dart
+++ lib/widgets/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:mental_health_application2/utils/app_layout.dart';
 import 'package:mental_health_application2/widgets/round_container.dart';
 
@@ -20,6 +21,7 @@ class TicketView extends StatelessWidget {
         // Contains whole card
         child: Column(
           children: <Widget>[
+            // Card blue part
             Container(
               padding: const EdgeInsets.all(16.0),
               decoration: const BoxDecoration(
@@ -39,41 +41,63 @@ class TicketView extends StatelessWidget {
 
                       const RoundContainer(),
                       Expanded(
-                        child: SizedBox(
-                          height: 24,
-                          child: LayoutBuilder(
-                            builder: (BuildContext context, BoxConstraints constraints) {
-                              return Flex(
-                                direction: Axis.horizontal,
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                mainAxisSize: MainAxisSize.max,
-                                children: List.generate(
-                                  // Generate more or less dashes depending on the screen size
-                                  (constraints.constrainWidth(5.0) / 6).floor(),
-                                  (index) => const SizedBox(
-                                    width: 3.0,
-                                    height: 1.0,
-                                    child: DecoratedBox(
-                                      decoration: BoxDecoration(color: Colors.white),
+                        // Used to overlap widgets
+                        child: Stack(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context, BoxConstraints constraints) {
+                                  return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List.generate(
+                                      // Generate more or less dashes depending on the screen size
+                                      (constraints.constrainWidth(double.infinity) / 6).floor(),
+                                      (index) => const SizedBox(
+                                        width: 3.0,
+                                        height: 1.0,
+                                        child: DecoratedBox(
+                                          decoration: BoxDecoration(color: Colors.white),
+                                        ),
+                                      ),
                                     ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Center(child: Transform.rotate(angle: 1.5, child: const Icon(Icons.local_airport_rounded, color: Colors.white))),
+                          ],
                         )
                       ),
-                      const Icon(Icons.local_airport_rounded, color: Colors.white),
                       const RoundContainer(),
 
                       const Spacer(),
 
-                      Text('London', style: Styles.headLine3Style.copyWith(color: Colors.white)),
+                      Text('LDN', style: Styles.headLine3Style.copyWith(color: Colors.white)),
+                    ],
+                  ),
+                  const Gap(3.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 100,
+                        child: Text('New-York', style: Styles.headLine4Style.copyWith(color: Colors.white)),
+                      ),
+                      Text('8H 30M', style: Styles.headLine4Style.copyWith(color: Colors.white)),
+                      SizedBox(
+                        width: 100,
+                        child: Text('London', textAlign: TextAlign.end, style: Styles.headLine4Style.copyWith(color: Colors.white)),
+                      ),
                     ],
                   ),
                 ],
               ),
-            )
+            ),
+            // Card orange part
+            Container(),
           ],
         ),
       ),
