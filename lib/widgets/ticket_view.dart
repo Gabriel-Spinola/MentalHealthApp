539893a4329528136e1431a070f77f4d439d8598
gseven-gdv <zerogravitystd@gmail.com>
2022-12-06 14:40:27 -0300

    TickView

--- lib/widgets/ticket_view.dart
+++ lib/widgets/ticket_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mental_health_application2/utils/app_layout.dart';
+import 'package:mental_health_application2/widgets/round_container.dart';
 
 import '../utils/app_styles.dart';
 
@@ -33,7 +34,40 @@ class TicketView extends StatelessWidget {
                   Row(
                     children: [
                       Text('NYC', style: Styles.headLine3Style.copyWith(color: Colors.white)),
+
                       const Spacer(),
+
+                      const RoundContainer(),
+                      Expanded(
+                        child: SizedBox(
+                          height: 24,
+                          child: LayoutBuilder(
+                            builder: (BuildContext context, BoxConstraints constraints) {
+                              return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.max,
+                                children: List.generate(
+                                  // Generate more or less dashes depending on the screen size
+                                  (constraints.constrainWidth(5.0) / 6).floor(),
+                                  (index) => const SizedBox(
+                                    width: 3.0,
+                                    height: 1.0,
+                                    child: DecoratedBox(
+                                      decoration: BoxDecoration(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      ),
+                      const Icon(Icons.local_airport_rounded, color: Colors.white),
+                      const RoundContainer(),
+
+                      const Spacer(),
+
                       Text('London', style: Styles.headLine3Style.copyWith(color: Colors.white)),
                     ],
                   ),
