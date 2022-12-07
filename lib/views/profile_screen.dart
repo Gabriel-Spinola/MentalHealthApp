1370cc1d255a8da714d9576b0e89dd780501a109
gseven-gdv <zerogravitystd@gmail.com>
2022-12-07 16:56:21 -0300

    starting profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_application2/utils/app_layout.dart';
+import 'package:mental_health_application2/utils/app_styles.dart';
+import 'package:path/path.dart';
 
 import '../Widgets/bottom_bar.dart';
 
@@ -13,10 +16,23 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFeeedf2),
-      body: Text("ProfileScreen"),
-      bottomNavigationBar: BottomBar(),
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: <Widget>[
+          Container(
+            width: 86,
+            height: 86,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(AppLayout.getHeight(10.0)),
+              image: DecorationImage(
+                image: AssetImage('assets/images/img_1.png'),
+              )
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: const BottomBar(),
     );
   }
 }
